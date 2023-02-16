126e4dcd59713e4241dd23cf773d9d961d1ddddc
Martin Mulama <martin.mulama@yahoo.com>
2023-02-16 12:29:44 +0000

    Updated 4-puts.c to match betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-	/**
- 	* main - Entry point
- 	*
- 	* Description: A c program that uses puts function to print out desired output
- 	*
- 	* Return: Always 0 (Success)
-	*/
 
-	int main(void)
-	{
+/**
+ * main - Entry  point
+ * Description: A program that uses puts function
+ * Return: Always 0 (Success
+ */
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-	}
+}
