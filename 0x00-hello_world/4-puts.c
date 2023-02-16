33720aa2e95e973a4290dd0032e6117bcbc25ad3
Martin Mulama <martin.mulama@yahoo.com>
2023-02-16 11:30:40 +0000

    modified 4-puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+	/**
+ 	* main - Entry point
+ 	*
+ 	* Description: A c program that uses puts function to print out desired output
+ 	*
+ 	* Return: Always 0 (Success)
+	*/
+
 	int main(void)
 	{
 	puts("\"Programming is like building a multilingual puzzle");
