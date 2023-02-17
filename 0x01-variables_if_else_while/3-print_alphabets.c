17244bd136467b557896cd84e24b9238ca2a0a87
Martin Mulama <martin.mulama@yahoo.com>
2023-02-17 12:40:13 +0000

    added c file 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A prints aout all alphabetical letters
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char az;
+	char AZ;
+
+	for (az = 'a'; az <= 'z'; az++)
+	{
+		putchar(az);
+	}
+	for (AZ = 'A'; AZ <= 'Z'; AZ++)
+	{
+		putchar(AZ);
+	}
+	putchar('\n');
+
+	return (0);
+}
