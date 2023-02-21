d0f23310925ddf7a7ece8d413735b6cf144e124d
Martin Mulama <martin.mulama@yahoo.com>
2023-02-21 14:50:11 +0000

    added file 4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - Makes the sum of two numbers
+ * @c: First operand
+ *
+ * Description: This is a longer description.
+ * Checks whther a letter is uppercase or lower case
+ * Return: The sum of the two parameters
+ */
+int _isalpha(int c)
+{
+	if (c == 'C' || c == 'c')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
