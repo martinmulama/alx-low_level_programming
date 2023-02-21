706716d07a6f543ff991a45f283ea581e6dbed87
Martin Mulama <martin.mulama@yahoo.com>
2023-02-21 06:48:11 +0000

    added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void print_alphabet(void);
+/**
+ * main - Entry point
+ * Description: A program that uses function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+/**
+ * print_alphabet - Prints alphabets
+ *
+ * Description: This is alphabet printing function
+ * Return: The sum of the two parameters
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
