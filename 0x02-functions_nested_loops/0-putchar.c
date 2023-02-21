52a2c5a6a376511c4efdaba92129d70f1dacf984
Martin Mulama <martin.mulama@yahoo.com>
2023-02-21 05:55:34 +0000

    Added file 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A program that uses printf
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
