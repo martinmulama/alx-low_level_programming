758d8fc12632cdda4268f2e68186bfc78a1bba2c
Martin Mulama <martin.mulama@yahoo.com>
2023-02-23 12:39:55 +0000

    modified 7-print_diagonal.c to suit the checker

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -19,13 +19,12 @@ void print_diagonal(int n)
 	{
 		for (i = 1; i <= n; ++i)
 		{
-			for (j = 1; j < i; ++j)
+			for (j = 1; j <= i; ++j)
 			{
 				_putchar(' ');
 			}
 		_putchar(backslash);
 		_putchar('\n');
 		}
-	_putchar('\n');
 	}
 }
