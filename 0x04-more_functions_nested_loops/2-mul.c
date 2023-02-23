3e3c3accf63347036261341364034b5942b5735e
Martin Mulama <martin.mulama@yahoo.com>
2023-02-23 07:42:37 +0000

    added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/**
+ * mul - multiplies two integers
+ *
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: return result
+*/
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
