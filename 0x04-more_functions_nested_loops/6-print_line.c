3014f2ab63e8e641e0d12afcfb78ac971579d971
Martin Mulama <martin.mulama@yahoo.com>
2023-02-23 11:04:13 +0000

    added 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_line - print a straight line in terminal
+ *
+ * @n: is the number of times the _ character
+ *     should be printed
+*/
+void print_line(int n)
+{
+	char underscore = '_';
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(underscore);
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -20,3 +20,4 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_to_14(void);
+void print_line(int n);
