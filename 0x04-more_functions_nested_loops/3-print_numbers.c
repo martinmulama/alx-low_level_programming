ab5980e6f1f4a59c3a7699d367776b9c3796d3b4
Martin Mulama <martin.mulama@yahoo.com>
2023-02-23 08:00:06 +0000

    added 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - print 0 - 9
+ *
+ * Return: Always 0 (Success)
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+
+}
