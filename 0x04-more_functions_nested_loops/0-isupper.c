77cf88a9899c2ad4c90e8a6217bb45b9fe67d23f
Martin Mulama <martin.mulama@yahoo.com>
2023-02-23 07:17:32 +0000

    modified to suit betty

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * _isupper - check if c is upper
+ *
+ * @c: input for alphabet
+ *
+ * Return: 1 if its uppercase, 0 if not
+*/
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
