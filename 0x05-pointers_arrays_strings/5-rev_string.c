ee37bee1f1b4116e5edbd69705bf046a01fb3d69
Martin Mulama <martin.mulama@yahoo.com>
2023-02-27 15:05:34 +0000

    added file 5-main.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string parameter input
+ *
+ * Return: Void
+*/
+void rev_string(char *s)
+{
+	int i, j;
+	char ch;
+
+	for (j = 0; s[j] != '\0'; j++)
+		;
+
+	for (i = 0; i < j / 2; i++)
+	{
+		ch = s[i];
+
+		s[i] = s[j - 1 - i];
+
+		s[j - 1 - i] = ch;
+	}
+}
