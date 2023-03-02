25f03cdb7de7d7c51dc2a20d0d9431bd44ee5fe9
Martin Mulama <martin.mulama@yahoo.com>
2023-03-02 09:28:07 +0000

    modified 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,6 +7,7 @@
 void print_number(int n)
 {
 	if (n < 0)
+	{
 	_putchar('-');
 		n = -n;
 	}
@@ -14,6 +15,5 @@ void print_number(int n)
 	{
 	print_number(n / 10);
 	}
-
 	_putchar(n % 10 + '0');
 }
