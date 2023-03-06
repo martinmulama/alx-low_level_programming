55352e987109cadfd1ce8cbd91eee178254098ef
Martin Mulama <martin.mulama@yahoo.com>
2023-03-06 17:15:34 +0000

    added file 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -6,3 +8,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
+void print_diagsums(int *a, int size);
